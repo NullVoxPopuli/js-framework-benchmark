c87b48d36d16eeef1bf0eb698889347c2327fae8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-05 18:01:49 -0500

    wip

--- frameworks/keyed/glimmer-2-main/vite.config.js
+++ frameworks/keyed/glimmer-2-main/vite.config.js
@@ -1,18 +1,18 @@
 import fs from 'node:fs';
 
 import { precompile } from '@glimmer/compiler';
-import { defineConfig, type Plugin } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [benchmark()],
 });
 
-function benchmark(): Plugin {
+function benchmark() {
   return {
     name: '@glimmer/benchmark',
     load(id) {
       /** @type {string | undefined} */
-      let result: string | undefined;
+      let result;
       if (id.endsWith('.hbs')) {
         const source = fs.readFileSync(id, 'utf8');
         const compiled = precompile(source);
