e26ee2ab51d04f7d429df76927e9f2ffbab65250
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-21 13:49:48 -0500

    Go full minimal

--- frameworks/keyed/ember/src/router.js
+++ frameworks/keyed/ember/src/router.js
@@ -2,9 +2,10 @@ import EmberRouter from '@ember/routing/router';
 
 export default class Router extends EmberRouter {
   location = 'history';
-  rootURL = '/frameworks/keyed/ember/';
+  rootURL = import.meta.env.VITE_NO_JSBF ? '/' : '/frameworks/keyed/ember/';
 }
 
 Router.map(function () {
   this.route('index.html');
 });
+pa
